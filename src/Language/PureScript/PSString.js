db4377dea03ba9c5273a93a8368a53f1d87882c1
Charles Taylor <charlestaylor95@gmail.com>
2023-10-07 14:43:59 -0400

    Use let instead of const (#2)

--- src/Language/PureScript/PSString.js
+++ src/Language/PureScript/PSString.js
@@ -1,7 +1,7 @@
 export const decodeUtf16BEImpl = (arrayOfUt16CodeUnits) => {
   const arrayBuf = new ArrayBuffer(2 * arrayOfUt16CodeUnits.length);
   const dataBuf = new DataView(arrayBuf);
-  for (const i = 0; i < arrayOfUt16CodeUnits.length; i++) {
+  for (let i = 0; i < arrayOfUt16CodeUnits.length; i++) {
     dataBuf.setUint16(2 * i, arrayOfUt16CodeUnits[i], false);
   }
   const decoder = new TextDecoder("utf-16be", { fatal: true, ignoreBOM: false });
